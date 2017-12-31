e7fcc6581ee61ee7d11c9b2bf150794a3c9da7ec
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2017-12-31 19:18:12 +0700

    welcome.blade  Changes to be committed: 	modified:   package.json 	new file:   resources/assets/js/App.vue 	modified:   resources/assets/js/app.js 	modified:   resources/views/welcome.blade.php

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,26 @@
+// App.vue
+
+<template>
+    <div class="container">
+        <div>
+            <transition name="fade">
+                <router-view></router-view>
+            </transition>
+        </div>
+    </div>
+</template>
+
+<style>
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+</style>
+
+<script>
+
+    export default{
+    }
+</script>
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,24 @@
+// app.js
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from 'vue';
 
-require('./bootstrap');
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
-window.Vue = require('vue');
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import App from './App.vue';
+import Example from './components/Example.vue';
 
-Vue.component('example', require('./components/Example.vue'));
+const routes = [
+  {
+      name: 'Example',
+      path: '/',
+      component: Example
+  }
+];
 
-const app = new Vue({
-    el: '#app'
-});
+const router = new VueRouter({ mode: 'history', routes: routes});
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
