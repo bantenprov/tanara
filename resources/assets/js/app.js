629b555729c0c2096ca3cec2dcb33df5fa500afb
etanasia <unme.loved@gmail.com>
2018-01-02 00:39:21 +0700

    update vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,18 +1,62 @@
-import 'bootstrap';
+// import 'bootstrap';
+//
+// (function ($) {
+//   'use strict';
+//
+//   $(function () {
+//
+//     //== Initialize Bootstrap tooltip with `data-toggle="tooltip"`.
+//
+//     $('[data-toggle="tooltip"]').tooltip();
+//
+//     //== Initialize Bootstrap popover with `data-toggle="popover"`.
+//
+//     $('[data-toggle="popover"]').popover();
+//
+//   });
+//
+// }(jQuery));
 
-(function ($) {
-  'use strict';
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
 
-  $(function () {
+require('./bootstrap');
+window.axios = require('axios');
+import Vue from 'vue';
+import routes from './routes.js';
 
-    //== Initialize Bootstrap tooltip with `data-toggle="tooltip"`.
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+ Vue.component('example', require('./components/Example.vue'));
 
-    $('[data-toggle="tooltip"]').tooltip();
+/**
+ * Vue Router
+ *
+ * @link http://router.vuejs.org/en/installation.html
+ */
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+const router = new VueRouter({
+    routes
+});
 
-    //== Initialize Bootstrap popover with `data-toggle="popover"`.
+// define routes for users
+// const routes = [
+// 	{
+// 		path: '/',
+// 		name: 'homepage',
+// 		component: require('./components/Example.vue')
+// 	}
+// ]
 
-    $('[data-toggle="popover"]').popover();
+// const router = new VueRouter({ routes });
 
-  });
-
-}(jQuery));
+const app = new Vue({
+  router
+}).$mount('#app');
