c3efe1382a1108c9b733c7b6265f06d51c2b79e0
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2018-01-02 07:41:41 +0700

    update biography  Changes to be committed: 	modified:   resources/views/pages/landing-page/home/grid-card.blade.php 	modified:   resources/views/pages/landing-page/home/image.blade.php

--- resources/views/pages/landing-page/home/grid-card.blade.php
+++ resources/views/pages/landing-page/home/grid-card.blade.php
@@ -4,27 +4,28 @@
             <div class="col-sm-4 mb-3">
                 <div class="card text-white bg-primary h-100">
                     <div class="card-body">
-                        <h5 class="card-title">Primary card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title">Biografi</h5>
+                        <p class="card-text">Syekh Nawawi lahir dalam tradisi keagamaan yang sangat kuat di Kampung Tanara, sebuah desa kecil di kecamatan Tirtayasa, Kabupaten Serang, Banten.</p>
                     </div>
                 </div>
             </div><!-- /col -->
             <div class="col-sm-4 mb-3">
                 <div class="card text-white bg-success h-100">
                     <div class="card-body">
-                        <h5 class="card-title">Success card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title">Pendidikan</h5>
+                        <p class="card-text">Sejak berusia lima tahun, Syekh Nawawi sudah mulai belajar ilmu agama Islam langsung dari ayahnya, Di usianya yang belum genap lima belas tahun, telah mengajar banyak orang</p>
                     </div>
                 </div>
             </div><!-- /col -->
             <div class="col-sm-4 mb-3">
                 <div class="card text-white bg-info h-100">
                     <div class="card-body">
-                        <h5 class="card-title">Info card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title">Peranan</h5>
+                        <p class="card-text">Nama Syekh Nawawi al-Bantani semakin masyhur ketika dia ditunjuk sebagai Imam Masjidil Haram, menggantikan Syaikh Achmad Khotib Al-Syambasi atau Syekh Ahmad Khatib Al-Minangkabawi.</p>
                     </div>
                 </div>
             </div><!-- /col -->
         </div><!-- /.row -->
     </div>
 </section>
+
