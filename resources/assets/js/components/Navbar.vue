ed48191387c651ae72093cc720344d05f181cdeb
Igoy Nawamreh <igoynawamreh@gmail.com>
2018-01-22 00:55:00 +0700

    remove classes

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="dashboard-navbar navbar navbar-expand-md navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark py-3">
     <router-link class="navbar-brand d-flex flex-row align-items-center text-uppercase" to="/" exact>
       <img class="mr-2" src="/images/logo.png" width="36" height="36">
       <span>Tanara</span>
