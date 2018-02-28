53ff7b98f182496853f2201e41c2e578236ca739
igoynawamreh <igoynawamreh@gmail.com>
2018-02-28 10:25:27 +0700

    Fixes #36: SweetAlert2: Unknown parameter "footer".

--- resources/assets/js/components/views/DemoVueSweetalert.vue
+++ resources/assets/js/components/views/DemoVueSweetalert.vue
@@ -21,7 +21,6 @@ export default {
         type: 'error',
         title: 'Oops...',
         text: 'Something went wrong!',
-        footer: '<a href>Why do I have this issue?</a>',
       });
     }
   }
